3f2d36ec5409e55006687a555f379ed981b5bbc1
Joel Navez CT <110367357+Navezjt@users.noreply.github.com>
2024-06-17 17:46:13 -0500

    Add files via upload

--- aaaaaa/conditional.py
+++ aaaaaa/conditional.py
@@ -1,16 +1,21 @@
 from __future__ import annotations
 
-from PIL import Image
+msg = "[-] ADetailer: WebUI versions below 1.6.0 are not supported."
 
 try:
-    from modules.processing import create_binary_mask
-except ImportError:
+    from modules.processing import create_binary_mask  # noqa: F401
+except ImportError as e:
+    raise RuntimeError(msg) from e
 
-    def create_binary_mask(image: Image.Image):
-        return image.convert("L")
+
+try:
+    from modules.ui_components import InputAccordion  # noqa: F401
+except ImportError as e:
+    raise RuntimeError(msg) from e
 
 
 try:
     from modules.sd_schedulers import schedulers
 except ImportError:
+    # webui < 1.9.0
     schedulers = []
